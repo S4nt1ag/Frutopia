492e87f8372eda76aa0773d44ae66d8d01ec1619
Santiago <gabrielsantiagoneves2@gmail.com>
2023-05-25 13:31:37 -0300

    Criação spring

---
+++ frutopia/src/main/java/com/grupoone/frutopia/FrutopiaApplication.java
@@ -0,0 +1,13 @@
+package com.grupoone.frutopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrutopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrutopiaApplication.class, args);
+	}
+
+}
---
+++ frutopia/src/test/java/com/grupoone/frutopia/FrutopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoone.frutopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrutopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
