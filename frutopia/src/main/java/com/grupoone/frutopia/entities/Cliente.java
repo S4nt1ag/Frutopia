ede84770f1bc92d6f65d4775d8f775610d58fd15
filipe-oliv95 <filipec.oliveira95@gmail.com>
2023-05-25 17:03:34 -0300

    Cliente Entitie

--- frutopia/src/main/java/com/grupoone/frutopia/entities/Cliente.java
+++ frutopia/src/main/java/com/grupoone/frutopia/entities/Cliente.java
@@ -1,69 +1,152 @@
 package com.grupoone.frutopia.entities;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idCliente")
 
 @Entity
-@Table(name = "categoria")
+@Table(name = "cliente")
 public class Cliente {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_categoria")
-	private Integer idCategoria;
+	@Column(name = "id_cliente")
+	private Integer idCliente;
 
 	@NotBlank
-	@Column(name = "nome")
-	private String nome;
+	@Email
+	@Column(name = "email")
+	private String email;
 
 	@NotBlank
-	@Column(name = "descricao")
-	private String descricao;
+	@Column(name = "nome_completo")
+	private String nomeCompleto;
+
+	@NotBlank
+	@Pattern(regexp = "^[0-9]{11}")
+	@Column(name = "cpf")
+	private String cpf;
+
+	@NotBlank
+	@Pattern(regexp = "^[0-9]{9,15}")
+	@Column(name = "telefone")
+	private String telefone;
+
+	@NotBlank
+	@Column(name = "data_nascimento")
+	private Date dataNascimento;
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedido;
+
+	@OneToOne
+	@JoinColumn(name = "id_endereco_FK", referencedColumnName = "id_endereco")
+	private Endereco endereco;
 
 	public Cliente() {
-
+		super();
 	}
 
-	public Cliente(Integer idCategoria, @NotBlank String nome, @NotBlank String descricao) {
-		this.idCategoria = idCategoria;
-		this.nome = nome;
-		this.descricao = descricao;
+	public Cliente(Integer idCliente, @NotBlank @Email String email, @NotBlank String nomeCompleto,
+			@NotBlank @Pattern(regexp = "^[0-9]{11}") String cpf,
+			@NotBlank @Pattern(regexp = "^[0-9]{9,15}") String telefone, @NotBlank Date dataNascimento,
+			List<Pedido> pedido, Endereco endereco) {
+		super();
+		this.idCliente = idCliente;
+		this.email = email;
+		this.nomeCompleto = nomeCompleto;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.dataNascimento = dataNascimento;
+		this.pedido = pedido;
+		this.endereco = endereco;
 	}
 
-	public Integer getIdCategoria() {
-		return idCategoria;
+	public Integer getIdCliente() {
+		return idCliente;
 	}
 
-	public void setIdCategoria(Integer idCategoria) {
-		this.idCategoria = idCategoria;
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
 	}
 
-	public String getNome() {
-		return nome;
+	public String getEmail() {
+		return email;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	public String getNomeCompleto() {
+		return nomeCompleto;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public List<Pedido> getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(List<Pedido> pedido) {
+		this.pedido = pedido;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(idCategoria);
+		return Objects.hash(idCliente);
 	}
 
 	@Override
@@ -75,7 +158,7 @@ public class Cliente {
 		if (getClass() != obj.getClass())
 			return false;
 		Cliente other = (Cliente) obj;
-		return Objects.equals(idCategoria, other.idCategoria);
+		return Objects.equals(idCliente, other.idCliente);
 	}
 
 }
