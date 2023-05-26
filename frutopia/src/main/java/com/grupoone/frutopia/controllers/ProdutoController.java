9bbbfd66ea711a982a743115192a580d3a3f2325
JoiceLisboa <joicepitzer@hotmail.com>
2023-05-25 22:06:12 -0300

    criação de produto

---
+++ frutopia/src/main/java/com/grupoone/frutopia/controllers/ProdutoController.java
@@ -0,0 +1,59 @@
+package com.grupoone.frutopia.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.grupoone.frutopia.entities.Categoria;
+import com.grupoone.frutopia.entities.Produto;
+import com.grupoone.frutopia.services.CategoriaService;
+import com.grupoone.frutopia.services.ProdutoService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+	@Autowired
+	ProdutoService produtoService;
+
+	@GetMapping
+	public ResponseEntity<List<Produto>> getAllProdutos() {
+		return new ResponseEntity<>(produtoService.getAllProdutos(), HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getProdutoById(@Valid @PathVariable Integer id) {
+		return new ResponseEntity<>(produtoService.getProdutoById(id), HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity<Produto> saveProduto(@Valid @RequestBody Produto produto) {
+		return new ResponseEntity<>(produtoService.saveProduto(produto), HttpStatus.CREATED);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Produto> updateProduto(@Valid @RequestBody Produto produto,
+			@Valid @PathVariable Integer id) {
+		return new ResponseEntity<>(produtoService.updateProduto(produto, id), HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> deleteProduto(@Valid @PathVariable Integer id) {
+		Boolean resp = produtoService.deleteProduto(id);
+		if (resp)
+			return new ResponseEntity<>(resp, HttpStatus.OK);
+		else
+			return new ResponseEntity<>(resp, HttpStatus.NOT_MODIFIED);
+	}
+}
---
+++ frutopia/src/main/java/com/grupoone/frutopia/entities/Produto.java
@@ -0,0 +1,171 @@
+package com.grupoone.frutopia.entities;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "idProduto"
+        )
+@Entity
+@Table(name = "produto")
+    public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_produto")
+	private Integer idProduto;
+
+	@NotBlank
+	@Column(name = "nome")
+	private String nome;
+
+	@NotBlank
+	@Column(name = "descricao", unique = true)
+	private String descricao;
+
+	@NotBlank
+	@Column(name = "qtd_estoque")
+	private Integer qtdEstoque;
+
+	@NotBlank
+	@Column(name = "data_cadastro")
+	private Date dataCadastro;
+
+	@NotBlank
+	@Column(name = "valor_unitario")
+	private Double valorUnitario;
+
+	@NotBlank
+	@Column(name = "imagem")
+	private String imagem;
+
+	@NotBlank
+	@ManyToOne
+	@JoinColumn(name = "id_categoria_FK", referencedColumnName = "id_categoria")
+	private Categoria categoria;
+
+	@OneToMany(mappedBy = "produto")
+	private List<ItemPedido> listaItensPedidos;
+
+	public Produto(Integer idProduto, @NotBlank String nome, @NotBlank String descricao, @NotBlank Integer qtdEstoque,
+			@NotBlank Date dataCadastro, @NotBlank Double valorUnitario, @NotBlank String imagem,
+			@NotBlank Categoria categoria) {
+		super();
+		this.idProduto = idProduto;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.qtdEstoque = qtdEstoque;
+		this.dataCadastro = dataCadastro;
+		this.valorUnitario = valorUnitario;
+		this.imagem = imagem;
+		this.categoria = categoria;
+		}
+
+	public Integer getIdProduto() {
+		return idProduto;
+	}
+
+	public void setIdProduto(Integer idProduto) {
+		this.idProduto = idProduto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+	public void setQtdEstoque(Integer qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(Double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public List<ItemPedido> getListaItensPedidos() {
+		return listaItensPedidos;
+	}
+
+	public void setListaItensPedidos(List<ItemPedido> listaItensPedidos) {
+		this.listaItensPedidos = listaItensPedidos;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idProduto);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(idProduto, other.idProduto);
+	}
+	
+}
+	
+	
+		
